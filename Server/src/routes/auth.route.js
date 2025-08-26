aebf5730e71903c237e4062c117d09f1aeec33f1
22110440 <157661962+22110440@users.noreply.github.com>
2025-08-26 21:10:06 +0700

    forget password otp

--- Server/src/routes/auth.route.js
+++ Server/src/routes/auth.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { startRegistration, verifyRegistration } = require('../controllers/auth.controller');
+const { startRegistration, verifyRegistration ,forgotPassword,verifyForgotPassword,changePassword} = require('../controllers/auth.controller');
+const { sendOtpEmail } = require('../services/mail.service');
 
 const router = express.Router();
 
 router.post('/register/start', startRegistration);
 router.post('/register/verify', verifyRegistration);
-
+rputer.post('forgot-password', forgotPassword);
+router.post('forgot-password/verify', verifyForgotPassword);
+router.put('forgot-password/changePassword', changePassword);
 module.exports = router;
